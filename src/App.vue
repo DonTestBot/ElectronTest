b8180c328ec5078b9b1486374b58422fcca440a3
DonDarude <danielvineshen@gmail.com>
2019-02-26 02:03:30 +0800

    version 0.4.0

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <v-toolbar app>
       <v-toolbar-title class="headline text-uppercase">
         <span>DIOFY MUDA MUDA</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <span class="font-weight-light">WRYYYYYYY!</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn
